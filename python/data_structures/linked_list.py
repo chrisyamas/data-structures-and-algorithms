3704a179964538711986a66b740e6785670ce232
Christopher Yamas <yamas.chris@gmail.com>
2022-05-02 16:04:52 -0400

    linked_list module created with methods

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,40 @@
+
 class LinkedList:
     """
-    Put docstring here
+    This module allows creation of a singly linked list by making nodes with set values and adding them to the beginning of the list.
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
+        self.list = []
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        str = ''
+        current = self.head
+        if self.head == None:
+            return 'NULL'
+        while current:
+            str += f"{{ {current.value} }} -> "
+            current = current.next
+        str += 'NULL'
+        return str.replace("'","")
+
+    def insert(self, value=None):
+        self.head = Node(value, self.head)
+
+    def includes(self, query):
+        current = self.head
+        while current:
+            if current.value == query:
+                return True
+            current = current.next
+        return False
+
+
+class Node:
+    def __init__(self, node_value=None, next_node=None):
+        self.value = node_value
+        self.next = next_node
 
 
 class TargetError:
