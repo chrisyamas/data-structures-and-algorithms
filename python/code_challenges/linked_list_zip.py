70b61799f9471e81f28b716b8c47643c72187077
Christopher Yamas <yamas.chris@gmail.com>
2022-05-10 21:49:32 -0400

    function written, tests pass

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,21 @@
 def zip_lists(a, b):
-    pass
+    a_current = a.head
+    b_current = b.head
+
+    if not a_current or not b_current:
+        if b_current:
+            return b
+        if a_current:
+            return a
+
+    while a_current and b_current:
+        if a_current or b_current:
+            a.insert_after(a_current.value, b_current.value)
+            a_current = a_current.next
+            b_current = b_current.next
+            if a_current.next:
+                a_current = a_current.next
+
+    print(b)
+    return a
+
