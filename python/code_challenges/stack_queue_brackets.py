714d49d5622ee240517e8f17bc9fb1a7b1100b10
Christopher Yamas <yamas.chris@gmail.com>
2022-05-11 20:37:29 -0400

    function written

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,16 @@
-from data_structures.queue import Queue
 
+def multi_bracket_validation(string):
+    brac_list = []
+    begin = '{[('
+    end = ')]}'
+    dic = { ']': '[', ')': '(', '}': '{' }
 
-def multi_bracket_validation():
-    pass
+    for str_ in string:
+        if str_ in begin:
+            brac_list.append(str_)
+        elif str_ in end:
+            if len(brac_list) == 0:
+                return False
+            if brac_list[-1] == dic[str_]:
+                brac_list.pop()
+    return len(brac_list) == 0
