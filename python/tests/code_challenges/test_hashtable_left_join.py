76852d6d2566b2af12f3e1ec3e37ef6386a97477
Christopher Yamas <yamas.chris@gmail.com>
2022-06-19 23:55:46 -0400

    test written and passed

--- python/tests/code_challenges/test_hashtable_left_join.py
+++ python/tests/code_challenges/test_hashtable_left_join.py
@@ -1,35 +1,34 @@
 import pytest
 from code_challenges.hashtable_left_join import left_join
+from data_structures.hashtable import Hashtable
 
 
 def test_exists():
     assert left_join
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_example():
-    synonyms = {
-        "diligent": "employed",
-        "fond": "enamored",
-        "guide": "usher",
-        "outfit": "garb",
-        "wrath": "anger",
-    }
-    antonyms = {
-        "diligent": "idle",
-        "fond": "averse",
-        "guide": "follow",
-        "flow": "jam",
-        "wrath": "delight",
-    }
+    synonyms = Hashtable()
+    synonyms.set("fond", "enamored")
+    synonyms.set("wrath", "anger")
+    synonyms.set("diligent", "employed")
+    synonyms.set("outfit", "garb")
+    synonyms.set("guide", "usher")
 
-    expected = [
-        ["fond", "enamored", "averse"],
-        ["wrath", "anger", "delight"],
-        ["diligent", "employed", "idle"],
-        ["outfit", "garb", "NONE"],
-        ["guide", "usher", "follow"],
-    ]
+    antonyms = Hashtable()
+    antonyms.set("fond", "averse")
+    antonyms.set("diligent", "idle")
+    antonyms.set("guide", "follow")
+    antonyms.set("flow", "jam")
+    antonyms.set("wrath", "delight")
+
+    expected = Hashtable()
+    expected.set("fond", ("enamored", "averse"))
+    expected.set("wrath", ("anger", "delight"))
+    expected.set("diligent", ("employed", "idle"))
+    expected.set("outfit", ("garb", None))
+    expected.set("guide", ("usher", "follow"))
 
     actual = left_join(synonyms, antonyms)
 
